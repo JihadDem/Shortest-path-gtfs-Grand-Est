3ba020a83ef60c8ed8e7f45406bc4811a26b7815
Jihad <jihadprog@hotmail.com>
2022-02-06 11:08:34 +0100

    Predecessors list + externaliser les modules

--- add_interest.py
+++ add_interest.py
@@ -1,21 +1,11 @@
-from math import sin, cos, sqrt, atan2, radians;
+from util.distances import distance;
 import csv;
 import os;
 import json;
 
+
 BASE_PATH = os.path.dirname(os.path.realpath(__file__)) + '/';
 
-def distance(lat1, lon1, lat2, lon2):
-    lat1 = radians(float(lat1));
-    lat2 = radians(float(lat2));
-    lon1 = radians(float(lon1));
-    lon2 = radians(float(lon2));
-    R = 6373.0 # approximate radius of earth in km
-    dlon = lon1 - lon2
-    dlat = lat1 - lat2
-    a = sin(dlat / 2)**2 + cos(lat2) * cos(lat1) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c;
 
 points = [
     ['PI: Cathédrale de Strasbourg', 48.858525, 2.294492],
